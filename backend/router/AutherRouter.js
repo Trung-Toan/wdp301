ceed0f65673d6d4dea2ec6315f9964c4429211e8
Tran Trung Toan <trantrungtoan17092003@gmail.com>
2025-09-07 17:03:16 +0700

    Register is done

--- backend/router/AutherRouter.js
+++ backend/router/AutherRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { login, profile, register, findUserByEmail, forgetPassword } = require("../controller/AuthenController");
+const { login, profile, register, findUserByEmail, forgetPassword, googleLogin } = require("../controller/AuthenController");
 const router = express.Router();
 const {verifyToken} = require("./../middleware/authMiddleware")
 
@@ -9,5 +9,6 @@ router.post("/user/register", register);
 router.post("/user/find_email", findUserByEmail);
 router.put("/user/forgot_password", forgetPassword);
 router.get("/user/profile", verifyToken, profile);
+router.post("/user/google-login", googleLogin);
 
 module.exports = router;
--- frontend/src/utility/send.mail.js
+++ frontend/src/utility/send.mail.js
@@ -2,14 +2,14 @@ import emailjs from 'emailjs-com';
 
 const sendEmail = (to, subject, message) => {
   return emailjs.send(
-    'service_nrr3tw2',
-    'template_abotd4t',
+    process.env.SERVICE_ID,
+    process.env.TEMPLATE_ID,
     {
       to: to,
       subject: subject,
       message: message,
     },
-    'OEj4kkgxdJXtWCV0_'
+    process.env.PUBLIC_KEY
   );
 };
 
