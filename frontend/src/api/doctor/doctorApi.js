f6627291a8658c14956bdd12afa65328efee9815
Nam Phong <phongnnhe176274@fpt.edu.vn>
2025-10-27 01:44:23 +0700

    update api get doctor top

--- frontend/src/api/doctor/doctorApi.js
+++ frontend/src/api/doctor/doctorApi.js
@@ -2,7 +2,7 @@ import { axiosInstance } from "../axiosInstance";
 
 export const doctorApi = {
   // Lấy bác sĩ top (nếu không truyền limit -> lấy tất cả)
-  getDoctorTop: (limit) =>
+  getDoctorTopNearMe: (limit) =>
     axiosInstance.get("/doctor/top/near-me", { params: { limit } }),
 
   // Lấy bác sĩ theo chuyên khoa
