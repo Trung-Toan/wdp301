c1211a1f662ecdad4aeaca6d869ac306516e6896
dduong28703 <d.duong28703@gmail.com>
2025-10-27 16:24:29 +0700

    add route create doctor

--- backend/src/router/admin_clinic/admin_clinic.routes.js
+++ backend/src/router/admin_clinic/admin_clinic.routes.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const { authRequired, roleRequired } = require("./../../middleware/auth");
 
 //tạo tài khoản bác sĩ
-router.get("/", authRequired, roleRequired("ADMIN_CLINIC"), (req, res) => {
-  res.send("Tạo tài khoản bác sĩ");
-});
+router.post(
+  "/account",
+  authRequired,
+  roleRequired("ADMIN_CLINIC"),
+  require("./../../controller/admin-clinic/admin-clinic.controller")
+    .createAccount
+);
+module.exports = router;
