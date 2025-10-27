20587c3bcfba03792d2954e7e627cf7b875e0e79
dduong28703 <d.duong28703@gmail.com>
2025-10-28 00:17:45 +0700

    add controller get clinic

--- backend/src/controller/admin_clinic/admin_clinic.controller.js
+++ backend/src/controller/admin_clinic/admin_clinic.controller.js
@@ -1,11 +1,22 @@
 const {
   createDoctor,
+  getClinicByAdmin
 } = require("../../service/admin_clinic/adminClinic.service");
 
 exports.createAccountDoctor = async (req, res, next) => {
   try {
     const data = await createDoctor(req.body);
     res.json({ success: true, total: data.length, data });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getClinicByAdmin = async (req, res, next) => {
+  try {
+    const accountId = req.user?.sub;
+    const result = await getClinicByAdmin(accountId);
+    res.status(result.ok ? 200 : 400).json(result);
   } catch (err) {
     next(err);
   }
