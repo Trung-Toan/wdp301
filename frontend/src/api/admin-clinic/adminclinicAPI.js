d13bacc9eba9e58eb4d376aec414fb516afa82a9
dduong28703 <d.duong28703@gmail.com>
2025-10-27 13:20:58 +0700

    add api create clinic

--- frontend/src/api/admin-clinic/adminclinicAPI.js
+++ frontend/src/api/admin-clinic/adminclinicAPI.js
@@ -2,9 +2,12 @@ import { axiosInstance } from "../axiosInstance";
 
 export const adminclinicAPI = {
   //lấy danh sách chuyên khoa
-    getAllSpecialties: () => {
-        return axiosInstance.get("/clinic-registration/specialties");
-    },
+  getAllSpecialties: () => {
+    return axiosInstance.get("/clinic-registration/specialties");
+  },
 
-    
+  //gửi yêu cầu đăng ký phòng khám
+  createRegistrationRequest: (data) => {
+    return axiosInstance.post("/clinic-registration/create", data);
+  },
 };
