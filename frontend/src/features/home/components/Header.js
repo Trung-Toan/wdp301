92b408d03efcc141bbcad6004aa29430aa77afac
Nam Phong <phongnnhe176274@fpt.edu.vn>
2025-10-28 22:44:40 +0700

    delete import not use

--- frontend/src/features/home/components/Header.js
+++ frontend/src/features/home/components/Header.js
@@ -1,5 +1,5 @@
 import { Calendar, Menu } from "lucide-react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useSessionStorage } from "../../../hooks/useSessionStorage";
 import { Dropdown } from "react-bootstrap";
