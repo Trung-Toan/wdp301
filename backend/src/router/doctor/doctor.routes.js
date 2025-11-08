0c14b05836d4b40bb1c1792c99d1dd35ebaf03c2
Tran Trung Toan <trantrungtoan17092003@gmail.com>
2025-11-09 01:23:14 +0700

    add dashboard và view feedback

--- backend/src/router/doctor/doctor.routes.js
+++ backend/src/router/doctor/doctor.routes.js
@@ -40,6 +40,8 @@ router.get(
   DoctorController.viewListPatients
 );
 
+router.get("/dashboard", authRequired,roleRequired("DOCTOR"), DoctorController.viewDashboard);
+
 // GET /patients/:patientId
 // view information patient by patientId
 router.get(
